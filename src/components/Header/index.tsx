c8cea63e7b57ca772e5954f350c417a7bce0d8dc
justin1508 <justin1508@hotmail.com>
2021-12-19 12:28:24 -0300

    prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
 import styles from './styles.module.scss';
 import { SignButton } from '../SignButton';
+import { ActiveLink } from '../ActiveLink'
 
 export function Header() {
-  return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="hjnews" />
-        <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
-        </nav>
-        <SignButton />
-      </div>
-    </header>
-  );
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<img src="/images/logo.svg" alt="hjnews" />
+				<nav>
+					<ActiveLink href="/" activeClassName={styles.active}>
+						<a>Home</a>
+					</ActiveLink>
+					<ActiveLink href="/posts" prefetch activeClassName={styles.active}>
+						<a>Posts</a>
+					</ActiveLink>
+				</nav>
+				<SignButton />
+			</div>
+		</header>
+	);
 }
