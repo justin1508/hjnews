a826a6e955082db564d464481ce03319c5b7a89c
justin1508 <justin1508@hotmail.com>
2022-05-24 07:04:27 -0300

    add jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,52 +5,52 @@ import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
 interface HomeProps {
-  product: {
-    priceId: string;
-    amount: number;
-  }
+	product: {
+		priceId: string;
+		amount: string;
+	}
 }
 
 export default function Home({ product }: HomeProps) {
-  return (
-    <>
-      <Head><title>Home | hj.news</title></Head>
+	return (
+		<>
+			<Head><title>Home | hj.news</title></Head>
 
-      <main className={styles.contentContainer}>
-        <section className={styles.hero}>
-          <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
-          <p>Get Access to all publications<br />
-            <span>for {product.amount} month</span>
-          </p>
-          <SubscribeButton priceId={product.priceId} />
-        </section>
-        <img src="/images/avatar.svg" alt="Girl coding" />
-      </main>
-    </>
-  )
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏 Hey, welcome</span>
+					<h1>News about the <span>React</span> world.</h1>
+					<p>Get Access to all publications<br />
+						<span>for {product.amount} month</span>
+					</p>
+					<SubscribeButton />
+				</section>
+				<img src="/images/avatar.svg" alt="Girl coding" />
+			</main>
+		</>
+	)
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1JxIPnFKobnOy9DWbLBH1csG')
+	const price = await stripe.prices.retrieve('price_1JxIPnFKobnOy9DWbLBH1csG')
 
-  const product = {
-    priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    }).format(price.unit_amount / 100),
-  };
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+		}).format(price.unit_amount / 100),
+	};
 
-  return {
-    props: {
-      product
-    },
-    revalidate: 60 * 60 * 24, // 24 hours
-    // cache do html para todos os usuarios
+	return {
+		props: {
+			product
+		},
+		revalidate: 60 * 60 * 24, // 24 hours
+		// cache do html para todos os usuarios
 
-    //client side: ex: comentarios de um post(carrega depois)
-    //server side ex: dados usuarios logado
-    //static site generation ex: G1 nao logado
-  }
+		//client side: ex: comentarios de um post(carrega depois)
+		//server side ex: dados usuarios logado
+		//static site generation ex: G1 nao logado
+	}
 }
